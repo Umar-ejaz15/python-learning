f8090ef31fc6a464a77b2f4be09ac820d6cf46ea
Umar Ejaz <umar.senpaiiii@gmail.com>
2025-02-26 05:11:56 -0800

    qfinal

--- questions solving/questions-solving.py
+++ questions solving/questions-solving.py
@@ -1,123 +1,158 @@
-# # 1. Accept two number and print greatest between them
-# num1 = int(input("Enter 1st number: "))
-# num2 = int(input("Enter 2nd number: "))
-# if num1 > num2:
-#     print(num1, "is greater than", num2)
-# else:
-#     print(num2, "is greater than", num1)
+# 1. Accept two number and print greatest between them
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+if num1 > num2:
+    print(num1, "is greater than", num2)
+else:
+    print(num2, "is greater than", num1)
 
 # 2. Accept the gender from user and display msg accordingly
 
-# G = input("Enter your gender (M/F): ")
-# if G == "M" or G == "m":
-#     print("You are male")
-# elif G == "F" or G == "f":
-#     print("You are female")
-# else:
-#     print("please enter your gender correctly")
+G = input("Enter your gender (M/F): ")
+if G == "M" or G == "m":
+    print("You are male")
+elif G == "F" or G == "f":
+    print("You are female")
+else:
+    print("please enter your gender correctly")
 
 # 3. Accept a number adn check if its even or odd
-# num = int(input("Enter a number: "))
-# if num % 2 == 0:
-#     print(num, "is even")
-# else:
-#     print(num, "is odd")
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
 
 # 4. Accept a name and age from user and check if its valid or not
-# name = input("Enter your name: ")
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print(name, "is eligible to vote")
-# else:
-#     print(name, "is not eligible to vote")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print(name, "is eligible to vote")
+else:
+    print(name, "is not eligible to vote")
 
 # 5. Accept a number and check if its leap year or not
 
-# year = int(input("Enter a year: "))
-# if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-#     print(year, "is a leap year")
-# else:
-#     print(year, "is not a leap year")
+year = int(input("Enter a year: "))
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
 
 # 6. Accept a alphabet and tell either its vowel or not
 
-# alphabet = input("Enter an alphabet: ")
-# if alphabet in "aeiouAEIOU":
-#     print(alphabet, "is a vowel")
-# else:
+alphabet = input("Enter an alphabet: ")
+if alphabet in "aeiouAEIOU":
+    print(alphabet, "is a vowel")
+else:
 
-#     print(alphabet, "is not a vowel")
+    print(alphabet, "is not a vowel")
 
 # 7. print natural number upto nth
 
-# num = int(input("please enter a number "))
-# for i in range(1,num+1):
-#     print(i)
+num = int(input("please enter a number "))
+for i in range(1, num+1):
+    print(i)
 
 # 8. reverse loop now
-# num = int(input("enter your number: "))
+num = int(input("enter your number: "))
 
-# for i in range(num, -1, -1):
-#     print(i)
+for i in range(num, -1, -1):
+    print(i)
 
 # 9. take a number as input and write it table
-# num = int(input("enter a num: "))
-# for i in range(1, 11):
-#     print(f"{num} *  {i} = {num*i}")
+num = int(input("enter a num: "))
+for i in range(1, 11):
+    print(f"{num} *  {i} = {num*i}")
 
 # 10. sum upto n term
-# num = int(input("enter a num:"))
-# sum = 0
-# for i in range(1, num):
-#     # print(num+i)
-#     sum = sum + i
+num = int(input("enter a num:"))
+sum = 0
+for i in range(1, num):
+    # print(num+i)
+    sum = sum + i
 
-# print(sum)
+print(sum)
 
 # 11. factorial of a number?
-# num = int(input("enter a num: "))
-# fact = 1
-# for i in range(num, 0, -1):
-#     fact = fact*i
+num = int(input("enter a num: "))
+fact = 1
+for i in range(num, 0, -1):
+    fact = fact*i
 
-# print(fact)
+print(fact)
 
 # 12. what are factors of a number
 
-# num = int(input("enter a num: "))
-# print("all factors are ")
-# for i in range(1, num+1):
-#     if num % i == 0:
-#         print(i, end=" ")
+num = int(input("enter a num: "))
+print("all factors are ")
+for i in range(1, num+1):
+    if num % i == 0:
+        print(i, end=" ")
 
 # 13. enter a number and check if its a perfect number or not
-# num = int(input("enter a num "))
-# sum = 0
-# for i in range(1, num):
-#     if num % i == 0:
-#         sum = sum + i
+num = int(input("enter a num "))
+sum = 0
+for i in range(1, num):
+    if num % i == 0:
+        sum = sum + i
 
 
-# if sum == num:
-#     print(f"{num} is a perfect number")
-# else:
-#     print(f"{num} is not a perfect number")
+if sum == num:
+    print(f"{num} is a perfect number")
+else:
+    print(f"{num} is not a perfect number")
 # 14. find prime number and composite number
-# num = int(input("enter a num "))
-# count = 0
-# for i in range(1, num+1):
-#     if num % i == 0:
-#         count = count + 1
-#         print(i)
+num = int(input("enter a num "))
+count = 0
+for i in range(1, num+1):
+    if num % i == 0:
+        count = count + 1
+        print(i)
 
-# print(count)
-# if (count == 2):
-#     print("prime number")
-# else:
-#     print("composite number")
+print(count)
+if (count == 2):
+    print("prime number")
+else:
+    print("composite number")
 
-# 15. sperate each digit of a number and check if its pallindroic number 
+# 15. sperate each digit of a number and check if its pallindroic number
+num = int(input("enter a num: "))
+copy = num
+rev = 0
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    # print(digit)
+    num = num // 10
 
-num = int(input("enter a number: "))
+
+print(rev)
+print(copy)
+
+if copy == rev:
+    print("pallindromic number")
+else:
+    print("not a pallindromic number")
 
 
+# 16. print right triangle
+
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print(" ", end="*")
+        # print(start=" ","*", end=" ")
+
+    print()
+
+
+# 17. print fabichoni series
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+
+# Example: Print first 10 Fibonacci numbers
+fibonacci(10)
